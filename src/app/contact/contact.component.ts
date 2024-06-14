5a682528c2e5c429e375b7334cd5460e3402144a
nyakotey <nnyakotey@gmail.com>
2024-06-14 10:36:58 +0000

    added emailjs service for contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -10,8 +11,27 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class ContactComponent {
-  constructor() {}
-  fullname = '';
-  email = '';
-  message = '';
+  fullname!: string;
+  email!: string;
+  message!: string;
+  sendEmail(event: { target: HTMLFormElement; }) {
+    emailjs
+      .sendForm('service_83amxb6', 'contact_form', event.target as HTMLFormElement, {
+        publicKey: 'rcyPcjrq8H0XHRGZI',
+      })
+      .then(
+        (res) => {
+          console.log('SUCCESS!', res.status, res.text);
+          this.fullname = '';
+          this.email = '';
+          this.message = '';
+          alert("message sent")
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
+  constructor() { }
+
 }
