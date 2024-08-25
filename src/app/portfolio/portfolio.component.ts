641a5b57cf6f4ab4852ebfdd4e8ea159b0bba0fa
nyakotey <nnyakotey@gmail.com>
2024-08-25 11:37:01 +0000

    update porfolio page update profile pic

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,7 +11,7 @@ export class PortfolioComponent {
   filter = 'all';
   isFilterActive = false;
   selectedCategory = 'Select category';
-  categories = ['All', 'ECE', 'AI', 'Web Development', 'CSS Art'];
+  categories = ['All', 'Engineering', 'AI/ML', 'Web Dev', 'CSS Art'];
 
   filterFunc(event: any) {
     this.filter = event.target.innerHTML.toLowerCase();
@@ -34,86 +34,79 @@ export class PortfolioComponent {
 
   projects = [
     {
-      title: 'CAD Designs with Fusion360',
-      category: 'ECE',
-      image: '../assets/images/project-9.png',
+      title: 'Musical Instrument Classification Transformer Model',
+      category: 'AI/ML',
+      image: '../assets/images/minst-classifier.png',
+      alt: 'AI/ML',
+      link: "https://huggingface.co/spaces/Nyakotey/Musical-Instrument-Classifier",
+    },
+    {
+      title: 'GPS Tracker - Design to PCB',
+      category: 'Engineering',
+      image: '../assets/images/gps.png',
+      alt: 'GPS Tracker - Design to PCB',
+      link: "/ece",
+    },
+    {
+      title: 'Todo App with Firebase',
+      category: 'Web Dev',
+      image: '../assets/images/todoapp.png',
+      alt: 'Todo App with Firebase',
+      link: "https://todosky6.netlify.app",
+    },
+    {
+      title: 'CAD Designs and modelling',
+      category: 'Engineering',
+      image: '../assets/images/cad.png',
       alt: 'Robot Gripper design in Fusion360',
       link: "/ece",
     },
     {
       title: 'Fuel to Electric Bike Conversion',
-      category: 'ECE',
-      image: '../assets/images/project-9.png',
+      category: 'Engineering',
+      image: '../assets/images/bike conversion.png',
       alt: 'Fuel to Electric Bike Conversion',
       link: "/ece",
     },
     {
-      title: 'EV Battery Build and Assembly',
-      category: 'ECE',
-      image: '../assets/images/project-9.png',
-      alt: 'EV Battery Build and Assembly',
+      title: 'EV Battery Lab Development',
+      category: 'Engineering',
+      image: '../assets/images/batterylab.png',
+      alt: 'EV Battery Lab Development',
       link: "/ece",
-    },
-    {
-      title: 'GPS Tracker - Design to PCB',
-      category: 'ECE',
-      image: '../assets/images/project-9.png',
-      alt: 'GPS Tracker - Design to PCB',
-      link: "/ece",
-    },
-    {
-      title: 'MATLAB Simulations',
-      category: 'ECE',
-      image: '../assets/images/project-9.png',
-      alt: 'MATLAB Simulations',
-      link: "/ece",
-    },
-    {
-      title: 'Musical Instrument Classification Transformer Model',
-      category: 'AI',
-      image: '../assets/images/project-7.png',
-      alt: 'AI',
-      link: "https://huggingface.co/spaces/Nyakotey/Musical-Instrument-Classifier",
     },
     {
       title: 'CallCodes',
-      category: 'Web Development',
-      image: '../assets/images/project-2.png',
+      category: 'Web Dev',
+      image: '../assets/images/callcodes.png',
       alt: 'callcodes',
       link: "https://call-codes.vercel.app/",
     },
     {
       title: 'Rcountdown',
-      category: 'Web Development',
-      image: '../assets/images/project-2.png',
+      category: 'Web Dev',
+      image: '../assets/images/countdown.png',
       alt: 'rcountdown',
       link: "https://rcountdown.netlify.app/",
     },
     {
-      title: 'Todo App with Firebase',
-      category: 'Web Development',
-      image: '../assets/images/project-2.png',
-      alt: 'Todo App with Firebase',
-      link: "https://todosky6.netlify.app",
-    },
-    {
       title: 'Coffin Dancers 👯‍♂️',
       category: 'CSS Art',
-      image: '../assets/images/project-4.png',
+      image: '../assets/images/coffin.png',
       alt: 'Coffin Dancers',
       link: "https://coffin-dance-meme.netlify.app",
     },
     {
-      title: 'Piano Man 🎹',
+      title: 'Loaders 🔃',
       category: 'CSS Art',
-      image: '../assets/images/project-4.png',
-      alt: 'piano man',
-      link: "https://www.sololearn.com/en/compiler-playground/Wa2geW5KnOIt",
+      image: '../assets/images/loaders.png',
+      alt: 'css loaders',
+      link: "https://www.sololearn.com/en/compiler-playground/WJQTaKV9LKoj",
     },
     {
       title: 'Epic Christmas Show🎅🎄🎬',
       category: 'CSS Art',
-      image: '../assets/images/project-4.png',
+      image: '../assets/images/xmas.png',
       alt: 'Epic Christmas Show',
       link: "https://www.sololearn.com/en/compiler-playground/WgboaWLHXidi",
     },
